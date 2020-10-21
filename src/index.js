4af8624fdcfa42341f47776d19d9578c9ea04f55
Emma <60733989+emma-r-slight@users.noreply.github.com>
2020-10-21 17:36:39 +1300

    get data from form to add to table

--- src/index.js
+++ src/index.js
@@ -1,40 +1,67 @@
-let libraryRow = document.querySelector('#displayBooks')
+let libraryRow = document.getElementById('bookTable')
+console.log(libraryRow)
 let form = document.querySelector('form')
-let mylibrary = []
+let myLibrary = []
 
-function Book(title, author, numPages, read) {
+function Book(title, author, numPages, read, reading) {
 	this.title = title
 	this.author = author
 	this.numPages = numPages
 	this.read = read
 }
 
-form.addEventListener('submit', handleBookForm)
-
-function handleBookForm(e) {
+form.addEventListener('submit', (e) => {
 	e.preventDefault()
 	const title = form.elements[0].value
 	const author = form.elements[1].value
 	const numPages = form.elements[2].value
-	const read = form.elements[3].value
-	const book = new Book(title, author, numPages, read)
-	addBookToLibrary()
-	form.reset()
-}
+	const read = form.elements[3].checked
 
-function addBookToLibrary(book) {
-	mylibrary.push(book)
-	updateLocalStorage()
+	const anItem = new Book(title, author, numPages, read)
+	addBookToLibrary(anItem)
+	form.reset()
+})
+
+function addBookToLibrary(anItem) {
+	myLibrary.push(anItem)
+	updateLocalStorage(myLibrary)
 	updateLibrary()
 }
 
 function updateLibrary() {
+	let localLibrary = getLibraryFromLocalStorage()
+	let idNum = 3
+
 	libraryRow.innerHTML = ''
-	mylibrary.forEach((item, index) => {
-		let book = document.createElement('tr')
+	localLibrary.forEach((item, index) => {
+		let libraryItem = document.createElement('tr')
+		libraryItem.setAttribute('data-index', index)
+		let beenRead
+		if (item.read) {
+			beenRead = 'checked'
+		}
+		libraryItem.innerHTML = `
+														<td>${idNum++}</td>
+														<td>${item.title}</td>
+														<td>${item.author}</td>
+														<td>${item.numPages}</td>
+														<td><input type="checkbox" name="hasRead" ${beenRead}></td>
+														`
+		libraryRow.appendChild(libraryItem)
 	})
 }
 
-function updateLocalStorage() {
+function getLibraryFromLocalStorage() {
+	try {
+		let localLibrary = JSON.parse(localStorage.getItem('myLibrary'))
+		console.log(localLibrary)
+		return localLibrary
+	} catch (e) {
+		console.log(e.code)
+		return false
+	}
+}
+
+function updateLocalStorage(myLibrary) {
 	localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
 }
