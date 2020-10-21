7ceb9baa42f0aa3b7b5d3b8e249b7002492c2bac
Emma <60733989+emma-r-slight@users.noreply.github.com>
2020-10-22 00:52:46 +1300

    add delete row function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 let libraryRow = document.getElementById('bookTable')
 console.log(libraryRow)
 let form = document.querySelector('form')
+
 let myLibrary = []
+let bookNum
 
 function Book(title, author, numPages, read, reading) {
 	this.title = title
@@ -12,6 +14,7 @@ function Book(title, author, numPages, read, reading) {
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault()
+
 	const title = form.elements[0].value
 	const author = form.elements[1].value
 	const numPages = form.elements[2].value
@@ -22,6 +25,13 @@ form.addEventListener('submit', (e) => {
 	form.reset()
 })
 
+function deleteRows() {
+	const remove_btns = document.querySelectorAll('.remove')
+	remove_btns.forEach((button) => {
+		button.addEventListener('click', removeBookFromLibrary)
+	})
+}
+
 function addBookToLibrary(anItem) {
 	myLibrary.push(anItem)
 	updateLocalStorage(myLibrary)
@@ -30,25 +40,30 @@ function addBookToLibrary(anItem) {
 
 function updateLibrary() {
 	let localLibrary = getLibraryFromLocalStorage()
-	let idNum = 3
 
 	libraryRow.innerHTML = ''
 	localLibrary.forEach((item, index) => {
 		let libraryItem = document.createElement('tr')
 		libraryItem.setAttribute('data-index', index)
+		bookNum = index + 1
 		let beenRead
 		if (item.read) {
 			beenRead = 'checked'
 		}
-		libraryItem.innerHTML = `
-														<td>${idNum++}</td>
-														<td>${item.title}</td>
-														<td>${item.author}</td>
-														<td>${item.numPages}</td>
-														<td><input type="checkbox" name="hasRead" ${beenRead}></td>
-														`
+		libraryItem.innerHTML = `<td >${bookNum}</td><td>${item.title}</td><td>${item.author}</td><td>${item.numPages}</td><td><input type="checkbox" name="hasRead" ${beenRead}></td><td><a class="remove" title="delete"><i class="fa fa-wrapper fa-times"></i></a><td>`
 		libraryRow.appendChild(libraryItem)
+		let index2 = e.target.parentNode.getAttribute('data-index')
+		console.log(index2)
+		deleteRows()
 	})
+}
+
+function removeBookFromLibrary(e) {
+	// Get data-index from parent div
+	let index = e.target.parentNode.getAttribute('data-index')
+	myLibrary.splice(index, 1)
+	updateLocalStorage()
+	updateLibrary()
 }
 
 function getLibraryFromLocalStorage() {
